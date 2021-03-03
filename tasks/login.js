9644a6a8cc5c81dc16872873245c013744b55e14
science-math-guy <adumont@mailbox.org>
2021-03-03 18:41:11 +0100

    update simple

--- tasks/login.js
+++ tasks/login.js
@@ -1,4 +1,13 @@
+require('dotenv').config();
+
 const chalk = require('chalk');
+
+const puppeteer = require('puppeteer-extra');
+
+const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+puppeteer.use(StealthPlugin());
+
+puppeteer.use(require('puppeteer-extra-plugin-anonymize-ua')());
 
 const login = async(page, username, password) => {
     await page.type('input[type=text]', username);
